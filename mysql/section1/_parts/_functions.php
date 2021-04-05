d97f8760ebe25dcf259f601cdd0eeec30cfc8ed1
篠崎裕太 <shnzk@ShinozMacBook.local>
2021-04-05 18:44:23 +0900

    [update]CRUDを実行できるWEBサイトを作成した

--- mysql/section1/_parts/_functions.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-function h($str)
-{
-  return htmlspecialchars($str, ENT_QUOTES, 'UTF-8');
-}
-
-define('SITE_URL', 'http://' . $_SERVER['HTTP_HOST'] . '/udemy/mysql/section1');
---
+++ php_mysql/section1/_parts/_functions.php
@@ -0,0 +1,8 @@
+<?php
+
+function h($str)
+{
+  return htmlspecialchars($str, ENT_QUOTES, 'UTF-8');
+}
+
+define('SITE_URL', 'http://' . $_SERVER['HTTP_HOST'] . '/udemy/php_mysql/section1');
